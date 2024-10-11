2b760226253f270963174082dc38fcb528f60b64
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-11 14:56:53 +0300

    Header tamamlandi

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SubHeader = () => {
+  return (
+    <View>
+      <Text>SubHeader</Text>
+    </View>
+  );
+};
+
+export default SubHeader;
