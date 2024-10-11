954e05627d1cdca2edcc8c73047328e2abfd1593
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-11 17:49:01 +0300

    Categories duzenlendi

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ import Router from "./src/navigation/Router";
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#9ee4d4" }}>
-      <StatusBar backgroundColor={"#9ee4d4"} barStyle={"dark-content"} />
+      <StatusBar barStyle={"dark-content"} />
       <Router />
     </SafeAreaView>
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import Header from "../components/Header";
 import SubHeader from "../components/SubHeader";
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   return (
@@ -8,6 +9,7 @@ const HomeScreen = () => {
       <ScrollView>
         <Header />
         <SubHeader />
+        <Categories />
       </ScrollView>
     </SafeAreaView>
   );
